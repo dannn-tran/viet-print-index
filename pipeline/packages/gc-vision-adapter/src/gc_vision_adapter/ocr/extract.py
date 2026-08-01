75fbddfb28419a3077147bce0433ba5944af78bd
dannn-tran <dannn.tran.w@gmail.com>
2026-08-01 16:23:27 +0800

    Simplify OCR text extraction concurrency

--- pipeline/packages/gc-vision-adapter/src/gc_vision_adapter/ocr/extract.py
+++ pipeline/packages/gc-vision-adapter/src/gc_vision_adapter/ocr/extract.py
@@ -1,39 +1,31 @@
 import json
 from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
 from pathlib import Path
 
 
 def get_ocr_fulltext_one(filepath: str | Path):
-    with open(filepath, 'r') as f:
-        data = json.load(f)
+    with open(filepath, encoding="utf-8") as handle:
+        data = json.load(handle)
 
-    return data['fullTextAnnotation']['text']
+    return data["fullTextAnnotation"]["text"]
 
 
 def extract_ocr_fulltext(src_dir: str, dst_dir: str, workers: int = 4):
     dst_dirpath = Path(dst_dir)
-    dst_dirpath.mkdir(exist_ok=True)
+    dst_dirpath.mkdir(parents=True, exist_ok=True)
 
     files = Path(src_dir).iterdir()
     if workers < 2:
-        for f in files:
-            _extract_ocr_fulltext_one(f, dst_dirpath)
+        for file in files:
+            _extract_ocr_fulltext_one(file, dst_dirpath)
         return
 
     with ThreadPoolExecutor(max_workers=workers) as executor:
-        for _ in executor.map(lambda file: _extract_ocr_fulltext_one(file, dst_dirpath), files):
-            pass
+        for _ in executor.map(_extract_ocr_fulltext_one, files, repeat(dst_dirpath)):
+            continue
 
 def _extract_ocr_fulltext_one(src: Path, dst_dirpath: Path):
     text = get_ocr_fulltext_one(src)
-
-    match src.name.rsplit('.', maxsplit=1):
-        case [x]:
-            filename = x
-        case [x, _]:
-            filename = x
-        case x:
-            raise Exception(f"Unexpected split: input={src.name}; output={x}")
-
-    dst = dst_dirpath / (filename + '.txt')
-    dst.write_text(text)
+    dst = dst_dirpath / f"{src.stem}.txt"
+    dst.write_text(text, encoding="utf-8")
